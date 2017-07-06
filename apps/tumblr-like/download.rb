6a0fa0b02154f4251536d7eba9177910079c3379
OTSUKA, Yuanying <yuanying@fraction.jp>
2017-07-06 11:55:37 +0900

    Make tumblr-like download videos

--- apps/tumblr-like/download.rb
+++ apps/tumblr-like/download.rb
@@ -3,7 +3,6 @@ require 'open-uri'
 require 'tumblr_client'
 require 'fileutils'
 
-year          = Date.today.strftime('%Y')
 script_dir    = File.expand_path(File.dirname(__FILE__))
 contents_path = File.join(script_dir, 'contents')
 
@@ -27,6 +26,84 @@ client          = Tumblr::Client.new
 offset          = 0
 saved_timestamp = nil
 
+def create_filepath(contents_path, current_timestamp, url, filename=nil)
+  time = Time.at(current_timestamp.to_i)
+  year = time.year
+  filename ||= File.basename(url)
+  filename = "#{9999999999999 - current_timestamp}-#{filename}"
+  filedir  = "#{contents_path}/#{year}"
+  FileUtils.mkdir_p(filedir) unless File.directory?(filedir)
+  filepath = "#{filedir}/#{filename}"
+
+  filepath
+end
+
+def download like, url, filepath
+  puts "url: #{url}"
+  puts "filepath: #{filepath}"
+  begin
+    unless File.exists?(filepath)
+      open(url) do |input|
+        open(filepath, 'w') do |output|
+          output.write input.read
+        end
+      end
+      sleep 1
+    else
+      puts "Skipped: #{File.basename(filepath)}"
+    end
+  rescue => ex
+    p ex
+    puts "Can't download #{like['post_url']}"
+  end
+end
+
+def download_photos(contents_path, like)
+  current_timestamp = like['liked_timestamp']
+
+  like['photos'].each do |photo|
+    url = photo['original_size']['url']
+    filepath = create_filepath(contents_path, current_timestamp, url)
+
+    download(like, url, filepath)
+  end
+rescue => ex
+  require 'pp'
+  pp like
+  pp ex
+end
+
+def download_video(contents_path, like)
+  current_timestamp = like['liked_timestamp']
+  video_id = like['id']
+
+  video_url = like['video_url']
+  video_ext = File.extname(video_url)
+  video_filename = "#{video_id}#{video_ext}"
+  video_filepath = create_filepath(
+    contents_path,
+    current_timestamp,
+    video_url,
+    video_filename,
+  )
+  download(like, video_url, video_filepath)
+  thumbnail_url = like['thumbnail_url']
+  thumbnail_ext = File.extname(thumbnail_url)
+  thumbnail_filename = "#{video_id}#{thumbnail_ext}"
+  thumbnail_filepath = create_filepath(
+    contents_path,
+    current_timestamp,
+    thumbnail_url,
+    thumbnail_filename,
+  )
+  download(like, thumbnail_url, thumbnail_filepath)
+rescue => ex
+  require 'pp'
+  pp like
+  p like['video_url']
+  pp ex
+end
+
 while true do
   likes = client.likes(offset: offset)
   likes['liked_posts'].each do |like|
@@ -42,31 +119,12 @@ while true do
     end
 
     puts "like! #{current_timestamp}"
-    next unless like['type'] == 'photo'
-    like['photos'].each do |photo|
-      begin
-        url = photo['original_size']['url']
-        filename = File.basename(url)
-        filename = "#{9999999999999 - current_timestamp}-#{filename}"
-        filedir  = "#{contents_path}/#{year}"
-        FileUtils.mkdir_p(filedir) unless File.directory?(filedir)
-        filepath = "#{filedir}/#{filename}"
-        unless File.exists?(filepath)
-          open(url) do |input|
-            open(filepath, 'w') do |output|
-              output.write input.read
-            end
-          end
-          sleep 1
-        else
-          puts "Skipped: #{filename}"
-        end
-      rescue => ex
-        p ex
-        puts "Can't download #{like['post_url']}"
-      end
+    if like['type'] == 'photo'
+      download_photos(contents_path, like)
+    elsif like['type'] == 'video'
+      download_video(contents_path, like)
     end
-    url = like['photos']
+
   end
 
   puts '========================================'
